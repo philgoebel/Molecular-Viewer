dbb6b990030abdcf43b9d37b23423df48a6e975a
Philip <>
2018-07-25 21:01:57 +1000

    Started Interations
    Created Interaction Script

---
+++ Assets/Interactions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour {
+	public GameObject Mol1;
+	public GameObject Mol2;
+	public GameObject Mol3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("1"))
+			{
+			Mol2.SetActive(false);
+			Mol3.SetActive(false);
+			Mol1.transform.Translate(6.0f,1.2f,0);
+
+		}
+
+	}
+}
